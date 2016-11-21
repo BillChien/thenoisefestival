aadaf4d6ce7e7fb9c04a6fabba7fa1037b11e9ea
Bill Chien <BillChien@Bills-MacBook-Pro.local>
2016-11-20 18:58:37 -0800

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,27 @@
 $('document').ready(function(){
+  $('#flex_container').mouseenter(
+    function(){
+    $('.content').fadeOut()}
+  )
+  $('.noise').mouseenter(
+    function(){
+    $('.content').fadeIn()}
+  )
+  $('.content').mouseleave(
+    function(){
+    $('.content').fadeOut()}
+  )
 
-  $('button').click(function() {
-    $('.announcement').toggle();
-  });
+  var image1 = 'images/phone.png';
+  var image2 = 'images/down.png';
 
-  $('.red-circle').mouseenter(function() {
-       $('.blue-square').fadeOut();
-  });
-
-  $('.red-circle').mouseleave(function() {
-       $('.blue-square').fadeIn();
-  });
-
-
-
-
+  $('.phone').click(function(){
+    $('.second_container .video').slideToggle({
+      direction: "up"}, 300);
+    $(this).prop('src', function (index, currentSource) {
+          return currentSource == image2 ? image1 : image2;
+    });
+  })
 
 
 
